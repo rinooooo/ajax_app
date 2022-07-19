e9dc5cc550b8d598ef4068846bd48c1082f91a9c
rinooooo <rino.0566@gmail.com>
2022-07-19 15:27:59 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,12 +2,19 @@ function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
+    //これ書かないと、javascriptからと、ブラウザからの2つのリクエストが→ブラウザからのリクエストを無効化
     const form = document.getElementById("form");
+    //要素を取得してform変数に代入
     const formData = new FormData(form);
+    //取得した要素(form)に入力された値を取得してformdata変数に代入
     const XHR = new XMLHttpRequest();
+    //XMLHttpRequestオブジェクト(リクエストを送信するのに必要)を生成してXHR変数に代入
     XHR.open("POST", "/posts", true);
+    //リクエストの内容を指定・・・open("HTTPメソッド", "パス", true)
     XHR.responseType = "json";
+    //レスポンスの形式(データフォーマット)を指定しておく(今回はJSON)
     XHR.send(formData);
+    //データ送信
   });
 };
 
