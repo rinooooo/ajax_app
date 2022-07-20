da097ca1d3077db32170b819c69a01d4ea728d66
rinooooo <rino.0566@gmail.com>
2022-07-20 16:32:26 +0900

    Basic認証を導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,8 @@ class PostsController < ApplicationController
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post}
+    #↑redirect_toだとページが更新されるから、eachメソッドの@postsのデータも更新される（増える）
+    # renderだとページ更新されないから、eachメソッドの@postsのデータはそのまま+挿入した投稿
+    # →ページ更新したタイミングでeachメソッドの@postsデータも更新される
   end
 end
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,6 @@
 const buildHTML = (XHR) => {
   const item = XHR.response.post;
+    //新しく作成されたデータ（レコード）
   const html = `
     <div class="post">
       <div class="post-date">
@@ -17,19 +18,20 @@ function post (){
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     //これ書かないと、javascriptからと、ブラウザからの2つのリクエストが→ブラウザからのリクエストを無効化
+    //↑form_with→createメソッドの流れのこと？
     const form = document.getElementById("form");
-    //要素を取得してform変数に代入
     const formData = new FormData(form);
-    //取得した要素(form)に入力された値を取得してformdata変数に代入
+    //取得した要素(form)に入力された値を取得
     const XHR = new XMLHttpRequest();
-    //XMLHttpRequestオブジェクト(リクエストを送信するのに必要)を生成してXHR変数に代入
+    //XMLHttpRequestオブジェクト(リクエストを送信するのに必要)を生成
     XHR.open("POST", "/posts", true);
-    //リクエストの内容を指定・・・open("HTTPメソッド", "パス", true)
+    //リクエストの内容を指定・・・open("HTTPメソッド", "パス", true) rails routesでposts#createのところみる？
     XHR.responseType = "json";
-    //レスポンスの形式(データフォーマット)を指定しておく(今回はJSON)
+    //レスポンスの形式(データフォーマット)を指定
     XHR.send(formData);
     //データ送信
     XHR.onload = () => {
+    //リクエスト送信が成功すると呼び出される
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
@@ -37,7 +39,9 @@ function post (){
       const list = document.getElementById("list");
       const formText = document.getElementById("content");
       list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      //データ挿入
       formText.value = "";
+      //フォーム入力欄を空に（ページ更新されないと一度記入したもの残ったままになってる）
     };
   });
 };
